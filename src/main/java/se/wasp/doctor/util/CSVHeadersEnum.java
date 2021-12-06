496f037f88bd0fed343a94266d4501b936a1316d
Deepika Tiwari <deepikatiwari92@gmail.com>
2021-12-06 13:08:14 +0100

    Add missing CSV headers

--- src/main/java/se/wasp/doctor/util/CSVHeadersEnum.java
+++ src/main/java/se/wasp/doctor/util/CSVHeadersEnum.java
@@ -1,7 +1,17 @@
 package se.wasp.doctor.util;
 
 public enum CSVHeadersEnum {
+    NAME,
     LINE_START,
     LINE_END,
+    COLUMN_START,
+    COLUMN_END,
     ABS_PATH,
+    VALUE,
+    PARENT_LINE_START,
+    PARENT_LINE_END,
+    PARENT_COLUMN_START,
+    PARENT_COLUMN_END,
+    PARENT_NAME,
+    VISIBILITY
 }
